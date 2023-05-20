a5392ad6dbfdbb85398315a4aba902ec69df44b2
Denkata-Iliev <denislav9711@gmail.com>
2023-05-20 20:17:50 +0300

    Dynamically generate secret number circles

--- scripts/modal.js
+++ scripts/modal.js
@@ -2,14 +2,16 @@ import { newGame } from "./test.js";
 
 //TODO change button id
 const modal = document.getElementById('number-of-digits-modal');
-const btn = document.getElementById('myBtn');
+const openNewGameModal = document.getElementById('open-new-game-modal-btn');
 const closeBtn = document.getElementById('modal-close-btn');
 const rangeInput = document.getElementById('digitsRangeInput');
 const newGameBtn = document.getElementById('new-game-btn');
+const rangeInputLabel = document.getElementById('range-label');
+const secretNumbersWrapper = document.getElementById('secret-number-wrap');
 
-rangeInput.oninput = () => document.getElementById('range-label').innerHTML = `Digits: ${rangeInput.value}`;
+rangeInput.oninput = () => rangeInputLabel.innerHTML = `Digits: ${rangeInput.value}`;
 
-btn.onclick = () => modal.style.display = 'block';
+openNewGameModal.onclick = () => modal.style.display = 'block';
 closeBtn.onclick = () => modal.style.display = 'none';
 
 // When the user clicks anywhere outside of the modal, close it
@@ -19,11 +21,27 @@ window.onclick = event => {
     }
 }
 
-let numberOfDigits = rangeInput.value;
+let numberOfDigits = parseInt(rangeInput.value);
 newGameBtn.onclick = () => {
     numberOfDigits = parseInt(rangeInput.value);
     newGame(numberOfDigits);
+    rangeInput.value = numberOfDigits;
+    rangeInputLabel.innerHTML = `Digits: ${rangeInput.value}`;
     modal.style.display = 'none';
+
+    createSecretNumberCircles(numberOfDigits);
 }
+
+function createSecretNumberCircles(number) {
+    secretNumbersWrapper.innerHTML = '';
+    for (let i = 0; i < number; i++) {
+        const span = document.createElement('span');
+        span.className = 'circle d-flex flex-column align-items-center justify-content-center';
+        span.innerHTML = '?';
+        secretNumbersWrapper.appendChild(span);
+    }
+}
+
+createSecretNumberCircles(numberOfDigits);
 
 export { numberOfDigits };
